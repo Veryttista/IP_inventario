ced11edf8314b03d368cf3d51d34aadbcc466a9d
Veryttista <122801590+Veryttista@users.noreply.github.com>
2025-01-21 17:55:47 -0400

    Update homeController.php

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -26,12 +26,16 @@ class homeController extends Controller
         ->orderBy(DB::raw('MONTH(fecha_hora)'))
         ->get();
         $cantidadmes = [];
-        for ($i=0;$i<12;$i++)  {
-            if ($mes[0]->mes==($i+1)) {
-               $cantidadmes[$i] = $mes[0]->cantidad;  
-            }else{
-                $cantidadmes[$i] =0;
-            }          
+        if ($mes->isEmpty()) {
+            $cantidadmes =0;
+        }else{
+            for ($i=0;$i<12;$i++)  {
+                if ($mes[0]->mes==($i+1)) {
+                $cantidadmes[$i] = $mes[0]->cantidad;  
+                }else{
+                    $cantidadmes[$i] =0;
+                }          
+            }
         }
         return view('inicio.home',compact('usuarios','personal','mantenimientos','equipos','cantidadmes'));
     }
